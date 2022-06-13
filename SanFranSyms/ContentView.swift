6f505f872cae69939def9b55ddfb229a80850901
AppRoach <codeapproach@gmail.com>
2022-06-13 10:22:25 +0200

    Initial Commit

---
+++ SanFranSyms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SanFranSyms
+//
+//  Created by Vadim Zhuk on 13.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SanFranSyms/SanFranSymsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanFranSymsApp.swift
+//  SanFranSyms
+//
+//  Created by Vadim Zhuk on 13.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SanFranSymsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
